c873036958eaf3dd89dc1f1a391a606089d3b750
K. Daukantas <kestasdauk@gmail.com>
2022-06-08 11:48:25 +0300

    first commit

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contacts = (props) => {
+  return (
+    <div>
+      <h2>vardas: {props.name}</h2>
+      <h3>email: </h3>
+      <h3>age: </h3>
+    </div>
+  );
+};
+
+export default Contacts;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Contacts from './Contacts';
+
+const Main = () => {
+  return (
+    <div>
+      <Contacts name={'Petras'} />
+    </div>
+  );
+};
+
+export default Main;
