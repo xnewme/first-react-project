dd17d75798c28c1ffc4ced4d9503188f40c4a444
K. Daukantas <kestasdauk@gmail.com>
2022-06-08 20:33:25 +0300

    added comments, posts, users

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import Contacts from './Contacts';
+import Posts from './Posts';
 
 const Main = () => {
   return (
     <div>
+      <Posts />
       <Contacts name={'Petras'} />
     </div>
   );
---
+++ src/components/Posts.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const Posts = () => {
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h3>{resourceType}</h3>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
+    </div>
+  );
+};
+
+export default Posts;
